c99059859869c1f69b35c68143a3ae06271cc728
Timur Meyster <timur.meyster@gmail.com>
2015-06-16 18:37:50 -0700

    added a primes method that returns n number of primes

--- lib/primes.rb
+++ lib/primes.rb
@@ -5,4 +5,25 @@ class Primes
     @primes = []
   end
 
+  def primes(n)
+    i = 2
+
+    while @primes.length < n
+      @primes << i if is_prime?(i)
+      i += 1
+    end
+    
+    @primes
+  end
+
+  private
+
+  def is_prime?(num)
+    (2...num).each do |divisor|
+      return false if num % divisor == 0
+    end
+
+    true
+  end
+
 end
